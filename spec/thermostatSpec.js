b09ed7caad20ac15025e03dc63d70663cd58c3b2
Sam Ixer <samantha.ixer@johnlewis.co.uk>
2019-04-03 14:41:38 +0100

    Not using fancy long commit messages

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -1,13 +1,18 @@
 describe("Thermostat", function(){
-  it("starts at 20 degrees", function(){
+  beforeEach(function() {
     thermostat = new Thermostat;
-    expect(thermostat.temperature).toEqual(20);
+  });
 
+  it("starts at 20 degrees", function(){
+    expect(thermostat.temperature).toEqual(20);
   });
 
   it("responds to an up function", function() {
-    thermostat = new Thermostat;
     expect(thermostat.up()).not.toBe(undefined);
   });
+
+  it("increases the temperature by 1 degree", function() {
+    expect(thermostat.up(1)).toEqual(21);
+  })
 
 });
